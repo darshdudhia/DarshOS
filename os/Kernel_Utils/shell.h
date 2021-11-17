71c3c2349f83f458aeb2257b1081ae0da28cad5d
darshdudhia <69112471+darshdudhia@users.noreply.github.com>
2021-11-17 20:28:43 +0530

    Update shell.h

--- os/Kernel_Utils/shell.h
+++ os/Kernel_Utils/shell.h
@@ -4,3 +4,4 @@ char command_exit[1000] = {'h', 'a', 'l', 't'};
 char command_reboot[1000] = {'r', 'e', 'b', 'o', 'o', 't'};
 char command_random[1000] = {'u', 'r', 'a', 'n', 'd', 'o', 'm'};
 char command_cls[1000] = {'c', 'l', 's'};
+char command_Admin[1000] = {'i',  ' ', 'a', 'm', ' ', 'r', 'o', 'o', 't'};
