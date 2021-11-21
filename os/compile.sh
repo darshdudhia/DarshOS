164294145e7e934c4eda8baf45fa550d3254de35
darshdudhia <69112471+darshdudhia@users.noreply.github.com>
2021-11-21 14:35:45 +0530

    Update compile.sh

--- os/compile.sh
+++ os/compile.sh
@@ -1,10 +1,10 @@
 nasm "bootloader.asm" -f bin -o "Bin/bootloader.bin"
 nasm "kernel_starter.asm" -f elf -o "Bin/kernel_starter.o"
-i386-elf-gcc -ffreestanding -m32 -g -c -lgcc "kernel.cpp" -o "Bin/kernel.o"
+nasm "interrupts/idt.asm" -f elf -o "Bin/idt.o"
+i386-elf-gcc -ffreestanding -m32 -mgeneral-regs-only -mno-mmx -mno-sse -mno-sse2 -mno-sse3 -mno-3dnow -mno-red-zone -g -c -lgcc "kernel.cpp" -o "Bin/kernel.o"
 nasm "zero.asm" -f bin -o "Bin/zero.bin"
+i386-elf-ld -o "Bin/full_kernel.bin" -Ttext 0x1000 -Trodata 0x1000 "Bin/kernel_starter.o" "Bin/kernel.o" "Bin/idt.o" --oformat binary
 
-i386-elf-ld -o "Bin/full_kernel.bin" -Ttext 0x1000 -Trodata 0x1000 "Bin/kernel_starter.o" "Bin/kernel.o" --oformat binary
+cat "Bin/bootloader.bin" "Bin/full_kernel.bin" "Bin/zero.bin" > "Bin/OS.bin"
 
-cat "Bin/bootloader.bin" "Bin/full_kernel.bin" "Bin/zero.bin"  > "Bin/OS.bin"
-
-qemu-system-x86_64 -drive format=raw,file="Bin/OS.bin",index=0,if=floppy,  -m 128M
+qemu-system-x86_64 --soundhw pcspk -drive format=raw,file="Bin/OS.bin",index=0,if=floppy,  -m 128M
